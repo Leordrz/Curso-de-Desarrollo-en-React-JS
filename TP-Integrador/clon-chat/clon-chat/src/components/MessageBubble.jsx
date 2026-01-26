189335eced329c5f82248055d384e1215d193928
leordrz <lrodriguez@frba.utn.edu.ar>
2026-01-26 19:56:32 -0300

    fix: eliminar carpeta clon-chat duplicada

--- TP-Integrador/clon-chat/clon-chat/src/components/MessageBubble.jsx
+++
@@ -1,8 +0,0 @@
-export default function MessageBubble({ mensaje }) {
-  return (
-    <div className={`message-bubble ${mensaje.from}`}>
-      <div className="message-text">{mensaje.text}</div>
-      <div className="message-time">{mensaje.time}</div>
-    </div>
-  );
-}
